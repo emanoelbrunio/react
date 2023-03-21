965bc11af1630c654bdacc6616a5619865da8e01
Emanoel Brunio <brunio700@gmail.com>
2023-03-20 21:38:51 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './error.page';
+import App from './App'
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+    },
+])
+
